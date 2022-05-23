2964adb23c995a5e4388a08510a30dc414af1d42
BarbosaLeandro <leandrofelipegb1@gmail.com>
2022-05-23 12:17:35 -0300

    CRUD para cliente implementada

--- src/main/java/com/valdir/helpdesk/domain/Cliente.java
+++ src/main/java/com/valdir/helpdesk/domain/Cliente.java
@@ -1,12 +1,16 @@
 package com.valdir.helpdesk.domain;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.valdir.helpdesk.domain.dtos.ClienteDTO;
 import com.valdir.helpdesk.domain.enums.Perfil;
 
 @Entity
@@ -16,6 +20,8 @@ public class Cliente extends Pessoa {
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	protected LocalDate dataCriacao = LocalDate.now();
 
 	public Cliente() {
 		super();
@@ -27,6 +33,19 @@ public class Cliente extends Pessoa {
 		addPerfil(Perfil.CLIENTE);
 	}
 
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+	
+	
+	
 	public List<Chamado> getChamados() {
 		return chamados;
 	}
